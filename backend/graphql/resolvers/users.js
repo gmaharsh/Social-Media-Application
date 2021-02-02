6dc9b885150798636e6e3843aefea9575e0cee9e
Maharsh <gmaharsh@yahoo.com>
2021-02-01 22:25:35 -0600

    Added Validators to check the fields for registration

--- backend/graphql/resolvers/users.js
+++ backend/graphql/resolvers/users.js
@@ -2,12 +2,27 @@ const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const {validateRegisterInput} = require('../../validators/validators')
 const { JWT_SECRET } = require('../../config');
+const { UserInputError } = require('apollo-server');
 
 module.exports = {
     Mutation: {
         async register(_, {registerInput:{username, email, password, confirmPassword}}, context, info) {
             //Validate User Data
+            const { valid, error } = validateRegisterInput(username, email, password, confirmPassword)
+            if (!valid) {
+                throw new UserInputError('Errors', {error})
+            }
+            const user = await User.findOne({ username });
+            if (user) {
+                throw new UserInputError('Username is taken', {
+                    error: {
+                        username:'This username is taken'
+                    }
+                })
+            }
+
             password = await bcrypt.hash(password, 12);
 
             const newUser = new User({
