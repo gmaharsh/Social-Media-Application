f53be739d8c8140d00ee7f564b263d0ee8da1c1c
Maharsh <gmaharsh@yahoo.com>
2021-02-02 10:28:45 -0600

    Created Delete Post Route

--- backend/graphql/resolvers/comments.js
+++ backend/graphql/resolvers/comments.js
@@ -1,5 +1,6 @@
 const Post = require('../../models/Post');
 const check_auth = require('../../validators/check_auth');
+const { AuthenticationError } = require('apollo-server');
 const { UserInputError } = require('apollo-server');
 
 module.exports = {
@@ -28,8 +29,25 @@ module.exports = {
             } else {
                 throw new UserInputError('Post not found')
             }
-
+        },
+        async deleteComment(_, { postId, commentId }, context){
+            const { username } = check_auth(context)
             
+            const post = await Post.findById(postId);
+
+            if (post) {
+                const commentIndex = post.comments.findIndex(c => c.id === commentId);
+
+                if (post.comments[commentIndex].username === username) {
+                    post.comments.splice(commentIndex, 1);
+                    await post.save();
+                    return post
+                } else {
+                    throw new AuthenticationError('Actions not allowed');
+                }
+            } else {
+                throw new UserInputError('Post not found')
+            }
         }
     }
 }
