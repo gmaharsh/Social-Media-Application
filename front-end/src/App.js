64605c669fa9dc2ba87f8fadf894a2e15ef1b597
Maharsh <gmaharsh@yahoo.com>
2021-02-02 16:44:04 -0600

    Added Semantic Ui and Routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {BrowserRouter as Router,Route} from "react-router-dom";
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react'
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
+import MenuBar from './Components/MenuBar/MenuBar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Lets Start</h1>
-    </div>
+    <Router>
+      <div className="ui container">
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </div>
+    </Router>
   );
 }
 
---
+++ front-end/src/Components/MenuBar/MenuBar.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { Menu } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
+
+function MenuBar() {
+  const [activeItem, setactiveItem] = useState('')
+
+  const handleItemClick = (e, { name }) => setactiveItem(name)
+
+    return (
+      <div>
+        <Menu pointing secondary>
+            <Menu.Item
+                name='home'
+                active={activeItem === 'home'}
+                onClick={handleItemClick}
+                as={Link}
+                to="/"
+            />
+            <Menu.Menu position='right'>
+            <Menu.Item
+                name='login'
+                active={activeItem === 'login'}
+                onClick={handleItemClick}
+                as={Link}
+                to="/login"
+            />
+            <Menu.Item
+                name='register'
+                active={activeItem === 'register'}
+                onClick={handleItemClick}
+                as={Link}
+                to="/register"
+            />
+          </Menu.Menu>
+        </Menu>
+      </div>
+    )
+}
+
+export default MenuBar
