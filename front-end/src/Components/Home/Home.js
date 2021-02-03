210804b371bd7ad0d7a380d91d637a26d436add8
Maharsh <gmaharsh@yahoo.com>
2021-02-03 11:36:24 -0600

    Created Registration Form

--- front-end/src/Components/Home/Home.js
+++ front-end/src/Components/Home/Home.js
@@ -1,29 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Transition } from 'semantic-ui-react';
 import Card from './Card/PostCard';
 
 function Home() {
-    const {loading, data: { getPosts: posts }} = useQuery(FETCH_POSTS_QUERY);
-
-    console.log("Posts", posts);
+    const FETCH_POSTS_QUERY = gql`
+    {
+        getPosts {
+        id
+        body
+        createdAt
+        username
+        likeCount
+        likes {
+            username
+        }
+        commentCount
+        comments {
+            id
+            username
+            createdAt
+            body
+        }
+        }
+    }`;
+    const { loading, data: { getPosts: posts } } = useQuery(FETCH_POSTS_QUERY);
     return (
-        <div className="home">
-            <Grid columns={3} divided>
-                <Grid.Row>
+        <div>
+            <Grid columns={3} divided >
+                <Grid.Row className="page_title">
                     <h1>Recent Posts</h1>
                 </Grid.Row>
                 <Grid.Row>
-                    {loading ? (
-                        <h1>Loading Posts</h1>
-                    ) : (
-                            posts && posts.map(post => (
-                            <Grid.Column key = {post.id} style = {{marginBottom:'20px'}}>
+                    {loading && <h1>Loading posts..</h1>}
+                    {!loading && (
+                        <Transition.Group>
+                            {posts &&
+                                posts.map((post) => (
+                                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
                                 <Card post={post} />
-                            </Grid.Column>
-                        )) 
+                                </Grid.Column>
+                            ))}
+                        </Transition.Group>
                     )}
                 </Grid.Row>
             </Grid>
@@ -31,26 +51,6 @@ function Home() {
     )
 }
 
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`;
+
 
 export default Home
--- front-end/src/Components/Register/Register.js
+++ front-end/src/Components/Register/Register.js
@@ -1,10 +1,62 @@
-import React from 'react'
-import './Register.css';
+import React, { useState } from 'react'
+import { Button, Form } from 'semantic-ui-react'
 
 function Register() {
+
+    const [values, setValue] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    })
+
+    const changeValues = (event) => {
+        setValue({...values, [event.target.name]: event.target.value})
+    }
+
+    const submitForm = (event) => {
+        event.preventDefault()
+    }
+
     return (
         <div className="register">
-            <h1>I am Register</h1>
+            <Form onSubmit={submitForm}>
+                <Form.Field>
+                    <Form.Input 
+                        label="Username"
+                        placeholder="Username..."
+                        name="username"
+                        type="text"
+                        values={values.username}
+                        onChange={changeValues}
+                    />
+                    <Form.Input 
+                        label="Email"
+                        placeholder="Email..."
+                        name="email"
+                        type="email"
+                        values={values.email}
+                        onChange={changeValues}
+                    />
+                    <Form.Input 
+                        label="Password"
+                        placeholder="Password..."
+                        name="password"
+                        type="password"
+                        values={values.password}
+                        onChange={changeValues}
+                    />
+                    <Form.Input 
+                        label="Confirm Password"
+                        placeholder="Confirm Password..."
+                        name="confirm password"
+                        type="password"
+                        values={values.confirmPassword}
+                        onChange={changeValues}
+                    />
+                </Form.Field>
+                <Button type='submit'>Submit</Button>
+            </Form>
         </div>
     )
 }
