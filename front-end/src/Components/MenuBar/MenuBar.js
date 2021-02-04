8c9133313ac77ecb3d323c87a28dc5e5c40a2952
Maharsh <gmaharsh@yahoo.com>
2021-02-03 18:11:48 -0600

    Added Logout Feature

--- front-end/src/Components/MenuBar/MenuBar.js
+++ front-end/src/Components/MenuBar/MenuBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 
@@ -6,16 +6,32 @@ import { AuthContext } from '../../context/auth';
 
 function MenuBar() { 
 
-    const { user, logout} = useContext(AuthContext)
-    const pathname = window.location.pathname;
-    const path = pathname === '/' ? 'home' : pathname.substr(1)
-    const [activeItem, setactiveItem] = useState(path)
+  const { user, logout} = useContext(AuthContext)
+  const pathname = window.location.pathname;
+  const path = pathname === '/' ? 'home' : pathname.substr(1)
+  const [activeItem, setactiveItem] = useState(path)
     
-    const handleItemClick = (e, { name }) => setactiveItem(name)
-
-    return (
-      <div>
-        <Menu pointing secondary size="massive" color="teal">
+  const handleItemClick = (e, { name }) => setactiveItem(name)
+  
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="teal">
+            <Menu.Item
+                name={user.username}
+                active
+                as={Link}
+                to="/"
+            />
+            <Menu.Menu position='right'>
+            <Menu.Item
+                name='logout'
+                onClick={logout}
+                as={Link}
+                to="/login"
+            />
+          </Menu.Menu>
+        </Menu>
+  ): (
+      <Menu pointing secondary size="massive" color="teal">
             <Menu.Item
                 name='home'
                 active={activeItem === 'home'}
@@ -40,8 +56,8 @@ function MenuBar() {
             />
           </Menu.Menu>
         </Menu>
-      </div>
-    )
+  )
+  return menuBar;
 }
 
 export default MenuBar
